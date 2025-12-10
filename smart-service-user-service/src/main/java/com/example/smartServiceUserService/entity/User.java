5c8f2e3e74c4f239f7f8e3d3be513754df212d21
rohit <owalrohit03@gmail.com>
2025-12-10 23:56:19 +0530

    user entity

--- smart-service-user-service/src/main/java/com/example/smartServiceUserService/entity/User.java
+++ smart-service-user-service/src/main/java/com/example/smartServiceUserService/entity/User.java
@@ -1,6 +1,9 @@
 package com.example.smartServiceUserService.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,8 +25,10 @@ public class User extends BaseEntity {
 	private String fisrtName;
 	private String lastName;
 	private String phoneNumber;
+	@Column(length = 50, unique = true) 
 	private String email;
 	private String password;
+	@Enumerated(EnumType.STRING) 
 	private Role role;
 	
 }
