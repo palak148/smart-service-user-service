0311af4705e1868032afc8370fa76674cd918bf5
Palak gupta <palak.gupta@innspire.com>
2025-12-10 17:27:57 +0530

    Intial Commit with project

---
+++ smart-service-user-service/src/main/java/com/example/smartServiceUserService/SmartServiceUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartServiceUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartServiceUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartServiceUserServiceApplication.class, args);
+	}
+
+}
